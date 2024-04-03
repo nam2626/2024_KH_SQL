63ff2750bec5dbe79ca8e88e1ed90ea8de6b73e9
elzenaro <nam2626@naver.com>
2024-04-03 10:34:56 +0900

    문제풀이

--- 20240402.sql
+++ 20240402.sql
@@ -157,14 +157,15 @@ WHERE CAR_MAKER LIKE 'BMW' OR CAR_MAKER LIKE 'Mercedes-Benz'
 OR CAR_MAKER LIKE 'Audi';
 --자동차 테이블에서 자동차금액이 7000이상 9000이하인 자동차를 조회
 SELECT * FROM CAR WHERE CAR_PRICE BETWEEN 7000 AND 9000;
---자동차 테이블에서 제조사가 Kia이면서 자동차금액이 7000이상 9000이하인 자동차를 조회
-
+--자동차 테이블에서 제조사가 Kia이면서 자동차금액이 70000이상 90000이하인 자동차를 조회
+SELECT * FROM CAR WHERE CAR_PRICE BETWEEN 70000 AND 90000 
+AND (CAR_MAKER LIKE 'Kia');
 --자동차 테이블에서 자동차번호가 3번째 자리가 8, 4번째 자리가 9인 자동차를 조회
-
+SELECT * FROM CAR WHERE CAR_ID LIKE '__89______';
 --자동차 테이블에서 금액이 10000넘는 자동차의 금액을 3000씩 금액을 낮추세요
-
+UPDATE CAR SET CAR_PRICE = CAR_PRICE - 3000 WHERE CAR_PRICE  >= 10000;
 --자동차 테이블에서 제조사가 Jeep인 데이터를 삭제하세요
-
+DELETE FROM CAR WHERE CAR_MAKER LIKE 'Jeep';
 
 
 
