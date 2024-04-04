54e64093033b4a03c0579bdb5695513ed38ba6dd
elzenaro <nam2626@naver.com>
2024-04-04 14:02:12 +0900

    그룹함수

--- 20240404.sql
+++ 20240404.sql
@@ -21,6 +21,31 @@ FROM NEW_STUDENT
 WHERE STD_SCORE >= 3
 GROUP BY STD_MAJOR; 
 
+SELECT STD_MAJOR, COUNT(*) AS MAJOR_COUNT 
+FROM NEW_STUDENT
+GROUP BY STD_MAJOR;
+
+--현재 학생 테이블에 있는 전체 점수 총합, 평균, 최대/최소값, 학생 수 조회
+SELECT SUM(STD_SCORE), AVG(STD_SCORE), MAX(STD_SCORE),
+	MIN(STD_SCORE) ,COUNT(*) 
+FROM NEW_STUDENT;
+
+--현재 학생 테이블에 있는 데이터를 기준으로 학과별, 인원수를 조회
+--단 조회하는 인원수가 3명 이상인 학과만 조회
+SELECT STD_MAJOR, COUNT(*) AS MAJOR_COUNT 
+FROM NEW_STUDENT
+GROUP BY STD_MAJOR 
+HAVING COUNT(*) >= 3 ;
+--학과별 인원수 조회시 학과 평균 점수가 2.0 이하인 학과만 조회
+SELECT STD_MAJOR, COUNT(*) AS MAJOR_COUNT, AVG(STD_SCORE) AS AVG_SCORE 
+FROM NEW_STUDENT
+GROUP BY STD_MAJOR 
+HAVING AVG(STD_SCORE) <= 2.0;
+
+--학생 테이블 제거 후 아래 테이블로 생성 후 샘플데이터 저장
+
+
+
 
 
 
