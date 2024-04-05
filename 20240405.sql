3db47650e7e2b1de04988d8bfcb8a07421689938
elzenaro <nam2626@naver.com>
2024-04-05 17:24:41 +0900

    조인

--- 20240405.sql
+++ 20240405.sql
@@ -238,10 +238,30 @@ UPDATE CAR SET CAR_MAKER_CODE =
 WHERE CAR_MAKER_NAME = CAR_MAKER);
 --CAR테이블의 CAR_MAKER 컬럼 삭제
 ALTER TABLE CAR DROP COLUMN CAR_MAKER;
+--자동차 정보 조회시 자동차 번호, 자동차 모델명, 제조사명, 제조년도, 금액
+SELECT C.CAR_ID, C.CAR_NAME, 
+	CM.CAR_MAKER_NAME, C.CAR_MAKE_YEAR, C.CAR_PRICE 
+FROM CAR C INNER JOIN CAR_MAKER CM
+ON C.CAR_MAKER_CODE = CM.CAR_MAKER_CODE; 
+--자동차 제조사별 자동차 제품 개수, 평균가, 최고가, 최소가 조회
+SELECT CM.CAR_MAKER_NAME, COUNT(*) AS MAKER_COUNT,
+MAX(C.CAR_PRICE) AS MAX_PRICE, MIN(C.CAR_PRICE) AS MIN_PRICE
+FROM CAR C INNER JOIN CAR_MAKER CM
+ON C.CAR_MAKER_CODE = CM.CAR_MAKER_CODE
+GROUP BY CM.CAR_MAKER_NAME; 
+--자동차 제조사별, 제조년도별, 출시된 제품 개수를 조회 
+--단, 금액이 10000이상인 것들만 대상으로 잡음
 
+--자동차 판매 정보 조회
+--판매 번호, 판매된 모델명, 판매일, 회원이름, 판매개수, 판매금액
 
+--자동차 판매 정보 조회
+--판매 번호, 판매된 모델명, 판매일, 회원이름, 판매개수, 판매금액
+--외부 조인을 이용해서 모든 자동차 데이터는 조회
 
+--판매 연도별, 제조사별, 판매 대수 총합, 판매금액 총합, 판매금액 평균을 조회
 
+--판매 연도/월별, 제조사별, 판매 대수 총합, 판매금액 총합, 판매금액 평균을 조회
 
-
-
+--한번도 판매되지 않은 자동차 목록 조회
+--자동차 번호, 자동차 모델명, 제조사명, 제조년도, 금액
